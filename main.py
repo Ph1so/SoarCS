4b8e230de91632913a3494813faa2abe3212e399
crispy-terr <114262617+crispy-terr@users.noreply.github.com>
2024-07-29 09:57:43 -0400

    Update main.py
    Added google api key

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 import os
+from googleapiclient.discovery import build
 
 load_dotenv()
 
 spotify_client_id = os.getenv('SPOTIFU_CLIENT_ID')
 spotify_client_secret = os.getenv('SPOTIFY_CLIENT_SECRET')
+
+yt_api_key = "AIzaSyDP9zn4N1N0eg2OZWrKYVHbLAqofjPtrnU"
+youtube = build("youtube", "v3", developerKey=yt_api_key)
 
